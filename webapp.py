c54bd4d4cff33d0f68be447a6a63ab83fdd9be56
goldenpaddler <goldenpaddler@yahoo.com>
2018-02-11 03:01:46 +0000

    update webapp with the signup

--- webapp.py
+++ webapp.py
@@ -15,7 +15,7 @@ def main():
 def showSignUp():
     return render_template('signup.html')
 
-"""
+
 @webapp.route('/signUp',methods=['POST'])
 def signUp():
     #read the posted values
@@ -23,10 +23,10 @@ def signUp():
     _email=request.form['inputEmail']
     _password=request.form['inputPassword']
 
-if _name and _email and _password:
-    return json.dump({'html':'<span>All fields are good!</span>'})
-else:
-    return json.dump({'html':'<span>Enter the required fields</span>'})
-"""
+    if _name and _email and _password:
+        return json.dumps({'html':'<span>All fields are good!</span>'})
+    else:
+        return json.dumps({'html':'<span>Enter the required fields</span>'})
+
 if __name__=="__main__":
     webapp.run()
